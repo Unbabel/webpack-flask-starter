688383b3e8613444d84f2b5f859e797708f43a7a
João Sardinha <joao.sardinha@unbabel.com>
2017-01-18 18:01:29 +0000

    Enable uglifyjs on production target

--- gulpfile.js
+++ gulpfile.js
@@ -152,6 +152,16 @@ const webpackConfig = require('./webpack.config.js');
 const webpackConfigExists = webpackConfig.entry;
 
 if (webpackConfigExists) {
+  if (targetIsProd) {
+    const webpackUglifyJsPlugin = new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        screw_ie8: true,
+        warnings: false,
+      },
+    });
+    webpackConfig.plugins.push(webpackUglifyJsPlugin);
+  }
+
   // Webpack
   gulp.task('build-webpack', (cb) => {
     webpack(webpackConfig, (err, stats) => {
