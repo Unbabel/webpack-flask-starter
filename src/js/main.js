50827bad46e51bd31e43e87e557a42dc7b9bcf8e
João Sardinha <joao.sardinha@unbabel.com>
2017-01-18 16:28:40 +0000

    Add jquery sample action

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-console.log('Unbabel Front-end Starter');
+/* global document */
+import $ from 'jquery';
+
+const reversibleMessage = $('.js-reversible-message');
+
+$(document).on('click', '.js-reverse-message', (e) => {
+  e.preventDefault();
+  let text = reversibleMessage.text();
+  let splitText = text.split('');
+  splitText = splitText.reverse();
+  text = splitText.join('');
+  reversibleMessage.text(text);
+});
