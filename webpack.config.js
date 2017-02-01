c3cfdf9aee2256e7c53b726829a43ee091a68819
João Sardinha <joao.sardinha@unbabel.com>
2017-02-01 15:17:07 +0000

    Remove lint from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
-var webpack = require("webpack");
+const webpack = require('webpack');
+
 module.exports = {
   context: path.join(__dirname, 'src', 'js'),
   resolve: {
-      modulesDirectories: ["bower_components"]
+    modulesDirectories: ['bower_components'],
   },
   entry: {
     main: './main.js',
@@ -26,7 +27,7 @@ module.exports = {
   },
   plugins: [
     new webpack.ResolverPlugin(
-        new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(".bower.json", ["main"])
-    )
-  ]
+        new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('.bower.json', ['main']),
+    ),
+  ],
 };
