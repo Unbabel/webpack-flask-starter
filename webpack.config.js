d4eb41ab5cca5d10b4ba5148afd2a8411ff96c53
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2018-06-19 15:59:21 +0100

    Add support for images in components

--- webpack.config.js
+++ webpack.config.js
@@ -63,6 +63,14 @@ module.exports = {
 					},
 				],
 			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					{
+						loader: 'file-loader',
+					},
+				],
+			},
 		],
 	},
 	devServer: {
