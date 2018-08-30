59642d5bb6e7fb1b3a5511837b112e6e2912973b
João Sardinha <johnsardine@gmail.com>
2018-08-30 19:36:34 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,14 @@ module.exports = {
 	entry: {
 		// all the files that you want to process or watch need to be here
 		// if this list gets too long, make a separate file and import it here
-		'css/basic': './project/static/src/scss/all.scss',
-		'js/home': './project/static/src/js/home.js',
-		'html/home': './project/templates/home.html',
+		basic: './project/static/src/scss/all.scss',
+		home: './project/static/src/js/home.js',
+		// 'html/home': './project/templates/home.html',
 	},
 	output: {
 		path: path.resolve(__dirname, './project/static/dist'),
+		publicPath: '/static/dist/',
+		filename: 'js/[name].bundle.js',
 	},
 	resolve: {
 		alias: {
@@ -37,6 +39,10 @@ module.exports = {
 			}),
 			new OptimizeCSSAssetsPlugin({}),
 		],
+		splitChunks: {
+			chunks: 'all',
+			name: 'common',
+		},
 	},
 	module: {
 		rules: [
@@ -58,7 +64,9 @@ module.exports = {
 				use: [
 					{
 						loader: 'html-loader',
-						options: { minimize: true },
+						options: {
+							minimize: true,
+						},
 					},
 				],
 			},
@@ -68,21 +76,11 @@ module.exports = {
 					// config for scss inside .vue files
 					{
 						resourceQuery: /^\?vue/,
-						use: [
-							'vue-style-loader',
-							'css-loader',
-							'sass-loader',
-						],
+						use: ['vue-style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
 					},
 					// config for stand-alone .scss files
 					{
-						use: [
-							'css-hot-loader',
-							MiniCssExtractPlugin.loader,
-							'css-loader',
-							'postcss-loader',
-							'sass-loader',
-						],
+						use: ['css-hot-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
 					},
 				],
 			},
@@ -92,12 +90,9 @@ module.exports = {
 					{
 						loader: 'url-loader',
 						options: {
-							name: '/static/src/img/[name].[ext]',
+							name: 'img/[name].[ext]',
 							limit: 10000,
 						},
-					},
-					{
-						loader: 'img-loader',
 					},
 				],
 			},
@@ -108,28 +103,34 @@ module.exports = {
 		// url used on the templates to get the static files
 		// example: /static/dist/file.css
 		publicPath: '/static/dist/',
+
 		// port where the dev server will start
-		port: 9001,
+		port: 8081, // Internal port
+		host: '0.0.0.0', // Docker-specific, allows being accessed by host
+
+		// Public url accessed by the host. Allows sockjs to work - localhost:7001
+		public: process.env.DEVELOPMENT_HOST_PORT,
+
 		// only logs stuff on the console when there's an error or a new compilation
 		stats: 'minimal',
 		proxy: {
 			// all the requests that are not /static/dist/ files go to flask
-			// 5000 is the default port for flask
+			// server:8080 is the docker-network address for the flask server
 			'!(/static/dist/**/**.*)': {
-				target: 'http://127.0.0.1:5000',
+				target: 'http://server:8080', // Matches internal docker network
 			},
 		},
 	},
 	plugins: [
-		new CopyWebpackPlugin([{
-			from: 'project/static/src/img',
-			to: '../dist/img',
-		}]),
-		new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
+		// new CopyWebpackPlugin([{
+		// 	from: 'project/static/src/img',
+		// 	to: '../dist/img',
+		// }]),
+		// new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
 		new MiniCssExtractPlugin({
 			// you can also put 'css/[name].css' to send the file to a /dist/css folder
-			filename: '[name].css',
-			chunkFilename: '[id].css',
+			filename: 'css/[name].css',
+			chunkFilename: 'css/[name].css',
 		}),
 		new WebpackShellPlugin({
 			// removes the /dist/html/ folder (that is not being used) after building
