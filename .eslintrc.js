fde0b5371b4e8f1b359d96d2b38ab36139802f1f
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2018-12-20 13:16:43 +0000

    Update the ESLint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,28 +5,28 @@ module.exports = {
         'plugin:vue/recommended',
         'airbnb-base'
     ],
-    'plugins': [
-        'vue',
+    plugins: [
+        'jest',
     ],
     'parser': 'vue-eslint-parser',
     'parserOptions': {
         'parser': 'babel-eslint',
     },
-    // add your custom rules here
     'rules': {
         'indent': ['error', 'tab'],
         'no-tabs': ['off'],
-        'brace-style': ['error', 'stroustrup'],
+        'brace-style': ['error', '1tbs'],
         'arrow-body-style': ['error', 'always'],
-        // allow debugger during development
         'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
         'import/extensions': [
             'error', 'always', {
                 'js': 'never',
-                'vue': 'never'
             }
         ],
         'vue/html-indent': ['error', 'tab'],
+        'vue/html-closing-bracket-newline': 'off',
+        'vue/max-attributes-per-line': 'off',
+        'vue/html-self-closing': 'off',
     },
     'settings': {
         'import/resolver': {
@@ -35,15 +35,9 @@ module.exports = {
             }
         }
     },
-    'globals': {
-        'test': true,
-        'expect': true,
-        'describe': true,
-        'window': true,
-        'beforeEach': true,
-        'history': true,
-        'document': true,
-        'jest': true,
-        'it': true,
+    'env': {
+        'es6': true,
+        'browser': true,
+        'jest/globals': true,
     },
 }
