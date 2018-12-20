89d3709f21f8dc190b672087c41e98e9b0e2d95b
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2018-12-20 18:16:23 +0000

    Fix a path in the test file

--- project/static/src/tests/specs/home_app.test.js
+++ project/static/src/tests/specs/home_app.test.js
@@ -1,6 +1,6 @@
 import { mount, createLocalVue } from '@vue/test-utils';
 // import Vuex from 'vuex';
-import HomeApp from '../../vue/apps/HomeApp.vue';
+import HomeApp from '../../apps/HomeApp.vue';
 
 const localVue = createLocalVue();
 // localVue.use(Vuex);
